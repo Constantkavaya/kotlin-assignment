a47dcce8888be6a3991d18c2bea75859fa2f53fa
Constantkavaya <kavayaconstant@gmail.comgit config --listgit config --global user.email kavayaconstant@gmail.com>
2021-02-21 00:03:31 +0300

    first commit

---
+++ src/hello.kt
@@ -0,0 +1,12 @@
+fun main(){
+   var name:String="sharonJulliett"
+    println(name)
+    var age:Byte=26
+    println(age)
+var weight:Float=56.6.toFloat()
+    println(weight)
+    var phoneNumber:String="0714057650"
+    println(phoneNumber)
+    var country:String="kenya"
+    println(country)
+}
